440fe5dfe2ad2e4e7556c66579421bfcdd85f2e5
Gustavoh345 <gustavohsilva345@gmail.com>
2026-03-19 22:03:46 -0300

    colocamos foto do logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
+
 import Logo_google from './assets/Logo_google.png'
 import Logo_apple from './assets/Logo_apple.webp'
+import Logo_omnimarket from './assets/Logo_omnimarket.jpg'
+import Logo_apple2 from './assets/Logo_apple2.jpg'
+
 import { Lock, Mail } from "lucide-react";
 
 
@@ -10,14 +14,14 @@ function App() {
     //tela de fundo
     <div className='w-screen h-screen bg-black m-0 '>
       <div className='p-6 flex flex-col justify-center'>
-        <img src="" alt="Logo omnimarket" className='flex justify-center'/>
+        <img src={Logo_omnimarket} alt="Logo omnimarket" className='mx-auto border border-yellow-400 h-[150px] w-[150px] hover:animate-spin'/>
         <h1 className='text-white flex justify-center text-[80px] hover:animate-spin'>OmniMarket</h1>
         <h4 className='text-white flex justify-center text-[20px]'>Compre e venda em um só lugar!</h4>
       </div>
 
       {/*div da parte central(inputs, botoes)*/}
       <div className="flex items-center justify-center bg-black">
-        <div className='bg-[#0f0f0f] rounded-3xl border-gray-950 flex justify-center h-[500px] w-[400px]' >
+        <div className='bg-[#0f0f0f] rounded-3xl border-gray-950 flex justify-center h-[500px] w-[400px] border border-yellow-400' >
     
           <div className='flex flex-col'>
             <div className='flex flex-col p-3'>
@@ -51,7 +55,7 @@ function App() {
             </button>
 
             <button className="flex items-center justify-center gap-3 w-full h-[44px] bg-black text-white rounded-xl border border-[#6B6B6B] hover:bg-neutral-900 transition">
-              <img src={Logo_apple} alt="Logo Apple" className="w-5 h-5"/>
+              <img src={Logo_apple2} alt="Logo Apple" className="w-5 h-5"/>
               <span className="font-medium">Continuar com a Apple</span>
             </button>
           </div>
